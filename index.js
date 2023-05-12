b4b58435f26419f228582cbf38479cdb032e8d19
Paulo Diego <paulo.diego@adm.unip.br>
2023-05-12 17:21:14 -0300

    handleSummary

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import GetCustomer from "./scenarios/Get-Customer.js";
 import { group, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js"
 
 
 export default () => {
@@ -9,3 +10,9 @@ export default () => {
 
     sleep(1);
 }
+
+export function handleSummary(data) {
+    return {
+        "resultados.html": htmlReport(data),
+    }
+};
